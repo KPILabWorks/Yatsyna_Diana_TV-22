e951cc3d4ac026b41038f3ade710fbc0acb792a4
aishokoo <yadianayat@gmail.com>
2025-03-06 13:13:11 +0200

    Додала програму

---
+++ pz1.py
@@ -0,0 +1,10 @@
+from itertools import combinations
+
+def all_combinations(lst):
+    for r in range(len(lst) + 1):
+        for comb in combinations(lst, r):
+            yield comb
+
+lst = [1,2, 3,4,5]
+for comb in all_combinations(lst):
+    print(comb)
